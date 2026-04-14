1653e2bc597310135052d272cc2a74168e86680b
Kevin Giovanny Ross Córdoba <kroscor010@g.educaand.es>
2026-04-14 08:50:08 +0200

    Redirigiendo la entrada a la app al login/registro

--- goal-app/src/app/index.tsx
+++ goal-app/src/app/index.tsx
@@ -12,6 +12,6 @@ import React from "react";
 export default function Index() {
   // Redirigir al flujo de auth (login/registro)
   return (
-    <Redirect href={('../(tabs)/matches') as RelativePathString} />
+    <Redirect href={('../auth') as RelativePathString} />
   );
 }
